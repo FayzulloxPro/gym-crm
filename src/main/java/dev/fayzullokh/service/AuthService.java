dfffe47eb2dfb8b780f0b7c2a786fe95a9111ea6
Fayzullox.Qaxramonov <qahramonovf26@gmail.com>
2024-03-04 16:57:52 +0500

    bye ipoteka

--- src/main/java/dev/fayzullokh/service/AuthService.java
+++ src/main/java/dev/fayzullokh/service/AuthService.java
@@ -58,7 +58,6 @@ public class AuthService {
         protectionService.loginSucceeded(username);
         return jwtTokenUtil.generateToken(username);
     }
-
     // for fun
     @Timed(value = "auth_service_refresh_token_time", description = "Time taken to refresh token")
     public TokenResponse refreshToken(RefreshTokenRequest refreshTokenRequest) {
